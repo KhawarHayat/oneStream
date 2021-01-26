b63a53b5c6882f1d0bd41a020c5aadf147db7735
Khawar Hayat <khawarhayat1997@gmail.com>
2021-01-26 16:02:47 +0500

    V 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+import React, { lazy, Suspense } from 'react'
 import './App.css';
-import Home from './Component/Home';
+// import Home from './Component/Home';
 import { Provider } from 'react-redux'
 import store from './Redux/store'
+import Loader from 'react-loader-spinner'
+const Home = lazy(() => import('./Component/Home.js'))
 function App() {
   return (
     <Provider store={store}>
+      <Suspense fallback={<div className='Loader'>
+        <Loader
+         type="Rings"
+         color="crimson"
+         height={500}
+         width={500}
+         timeout={3000} //3 secs
+ 
+      />
+      </div>}>
       <div className="App">
         <Home />
       </div>
+      </Suspense>
     </Provider>
 
   );
